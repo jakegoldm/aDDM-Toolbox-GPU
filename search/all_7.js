cb7149dc90dab9fc7c9955909c91c787558d736f
jakegoldm <jakegoldm@users.noreply.github.com>
2023-07-21 22:23:37 +0000

    deploy: 507e64ac232443f0934f729419b70061b911ce51

--- search/all_7.js
+++ search/all_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['latencies_0',['latencies',['../class_fixation_data.html#a07031fd2270b2693fecfb8121d246a74',1,'FixationData']]],
-  ['loaddatafromcsv_1',['loadDataFromCSV',['../util_8h.html#a78fc1c7aadc9bd3f08e29b796632b49b',1,'loadDataFromCSV(std::string expDataFilename, std::string fixDataFilename):&#160;util.cpp'],['../util_8cpp.html#a78fc1c7aadc9bd3f08e29b796632b49b',1,'loadDataFromCSV(std::string expDataFilename, std::string fixDataFilename):&#160;util.cpp']]]
+  ['likelihood_1',['likelihood',['../class_d_d_m_trial.html#acfd91cc904fe00b7a559b5d6d25ed5e0',1,'DDMTrial']]],
+  ['loaddatafromcsv_2',['loadDataFromCSV',['../util_8h.html#a78fc1c7aadc9bd3f08e29b796632b49b',1,'loadDataFromCSV(std::string expDataFilename, std::string fixDataFilename):&#160;util.cpp'],['../util_8cpp.html#a78fc1c7aadc9bd3f08e29b796632b49b',1,'loadDataFromCSV(std::string expDataFilename, std::string fixDataFilename):&#160;util.cpp']]]
 ];
--- search/variables_4.js
+++ search/variables_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['latencies_0',['latencies',['../class_fixation_data.html#a07031fd2270b2693fecfb8121d246a74',1,'FixationData']]]
+  ['latencies_0',['latencies',['../class_fixation_data.html#a07031fd2270b2693fecfb8121d246a74',1,'FixationData']]],
+  ['likelihood_1',['likelihood',['../class_d_d_m_trial.html#acfd91cc904fe00b7a559b5d6d25ed5e0',1,'DDMTrial']]]
 ];
