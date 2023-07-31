e51915f34c9af5e37970e0b227ce5a8328bcd922
jakegoldm <jakegoldm@users.noreply.github.com>
2023-07-31 23:29:20 +0000

    deploy: 73240f9b82b1fb9c30bfcba8b7ae481b5a54a8a6

--- search/all_8.js
+++ search/all_8.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['latencies_0',['latencies',['../class_fixation_data.html#a07031fd2270b2693fecfb8121d246a74',1,'FixationData']]],
   ['loaddatafromcsv_1',['loadDataFromCSV',['../util_8h.html#a78fc1c7aadc9bd3f08e29b796632b49b',1,'loadDataFromCSV(std::string expDataFilename, std::string fixDataFilename):&#160;util.cpp'],['../util_8cpp.html#a78fc1c7aadc9bd3f08e29b796632b49b',1,'loadDataFromCSV(std::string expDataFilename, std::string fixDataFilename):&#160;util.cpp']]],
-  ['loadtrialsfromcsv_2',['loadTrialsFromCSV',['../classa_d_d_m_trial.html#a370280f496885eeb71c2530d77f5d889',1,'aDDMTrial::loadTrialsFromCSV()'],['../class_d_d_m_trial.html#aa29a09c2bd047f0e7d15ccbd7bd6e7f3',1,'DDMTrial::loadTrialsFromCSV()']]]
+  ['loaddatafromsinglecsv_2',['loadDataFromSingleCSV',['../util_8h.html#ab6b9c5e80a13d7b17e7617f31e47f156',1,'loadDataFromSingleCSV(std::string filename):&#160;util.cpp'],['../util_8cpp.html#ab6b9c5e80a13d7b17e7617f31e47f156',1,'loadDataFromSingleCSV(std::string filename):&#160;util.cpp']]],
+  ['loadtrialsfromcsv_3',['loadTrialsFromCSV',['../classa_d_d_m_trial.html#a370280f496885eeb71c2530d77f5d889',1,'aDDMTrial::loadTrialsFromCSV()'],['../class_d_d_m_trial.html#aa29a09c2bd047f0e7d15ccbd7bd6e7f3',1,'DDMTrial::loadTrialsFromCSV()']]]
 ];
