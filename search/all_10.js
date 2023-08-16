66d744362eea8251cabb0fabb60dc21ba28ecfb5
jakegoldm <jakegoldm@users.noreply.github.com>
2023-08-16 19:43:46 +0000

    deploy: 066b56fd8d885a131d7d61b08f5a6a9daa36d4ba

--- search/all_10.js
+++ search/all_10.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['rangebias_0',['rangeBias',['../ddm__mle__bias_8cpp.html#abbf2c2f87b0dbece561f6f712cf953a4',1,'ddm_mle_bias.cpp']]],
-  ['ranged_1',['rangeD',['../addm__mle_8cpp.html#a800d6969137d46bac6c127a9f70b7e51',1,'rangeD:&#160;addm_mle.cpp'],['../benchmark_8cpp.html#a800d6969137d46bac6c127a9f70b7e51',1,'rangeD:&#160;benchmark.cpp'],['../ddm__mle_8cpp.html#a800d6969137d46bac6c127a9f70b7e51',1,'rangeD:&#160;ddm_mle.cpp'],['../ddm__mle__bias_8cpp.html#a800d6969137d46bac6c127a9f70b7e51',1,'rangeD:&#160;ddm_mle_bias.cpp']]],
-  ['rangesigma_2',['rangeSigma',['../addm__mle_8cpp.html#a347dbb7726ce6ea89520f5980d686dd8',1,'rangeSigma:&#160;addm_mle.cpp'],['../benchmark_8cpp.html#a347dbb7726ce6ea89520f5980d686dd8',1,'rangeSigma:&#160;benchmark.cpp'],['../ddm__mle_8cpp.html#a347dbb7726ce6ea89520f5980d686dd8',1,'rangeSigma:&#160;ddm_mle.cpp'],['../ddm__mle__bias_8cpp.html#a347dbb7726ce6ea89520f5980d686dd8',1,'rangeSigma:&#160;ddm_mle_bias.cpp']]],
-  ['rangetheta_3',['rangeTheta',['../addm__mle_8cpp.html#ad2aa7a364e6ed4961ee3bb151d523d2c',1,'rangeTheta:&#160;addm_mle.cpp'],['../benchmark_8cpp.html#ad2aa7a364e6ed4961ee3bb151d523d2c',1,'rangeTheta:&#160;benchmark.cpp']]],
+  ['rangebias_0',['rangeBias',['../addm__mle_8cpp.html#abbf2c2f87b0dbece561f6f712cf953a4',1,'rangeBias:&#160;addm_mle.cpp'],['../ddm__mle__bias_8cpp.html#abbf2c2f87b0dbece561f6f712cf953a4',1,'rangeBias:&#160;ddm_mle_bias.cpp']]],
+  ['ranged_1',['rangeD',['../addm__mle_8cpp.html#a800d6969137d46bac6c127a9f70b7e51',1,'rangeD:&#160;addm_mle.cpp'],['../ddm__mle_8cpp.html#a800d6969137d46bac6c127a9f70b7e51',1,'rangeD:&#160;ddm_mle.cpp'],['../ddm__mle__bias_8cpp.html#a800d6969137d46bac6c127a9f70b7e51',1,'rangeD:&#160;ddm_mle_bias.cpp']]],
+  ['rangesigma_2',['rangeSigma',['../addm__mle_8cpp.html#a347dbb7726ce6ea89520f5980d686dd8',1,'rangeSigma:&#160;addm_mle.cpp'],['../ddm__mle_8cpp.html#a347dbb7726ce6ea89520f5980d686dd8',1,'rangeSigma:&#160;ddm_mle.cpp'],['../ddm__mle__bias_8cpp.html#a347dbb7726ce6ea89520f5980d686dd8',1,'rangeSigma:&#160;ddm_mle_bias.cpp']]],
+  ['rangetheta_3',['rangeTheta',['../addm__mle_8cpp.html#ad2aa7a364e6ed4961ee3bb151d523d2c',1,'addm_mle.cpp']]],
   ['rdv_5ftime_4',['rdv_time',['../namespacerdv__time.html',1,'']]],
   ['rdv_5ftime_2epy_5',['rdv_time.py',['../rdv__time_8py.html',1,'']]],
   ['rdvs_6',['RDVs',['../class_d_d_m_trial.html#afe24a94d0425b430c2c1e4537091f35b',1,'DDMTrial']]],
--- search/functions_5.js
+++ search/functions_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['fitmodelmle_0',['fitModelMLE',['../classa_d_d_m.html#a2b9be9ecd601d05d8d935a4902c2f473',1,'aDDM::fitModelMLE()'],['../class_d_d_m.html#af6f4cab6e2175c4864222709f0d6bad4',1,'DDM::fitModelMLE()']]],
+  ['fitmodelmle_0',['fitModelMLE',['../classa_d_d_m.html#ab798056d5ea0636fa1cff32140067450',1,'aDDM::fitModelMLE()'],['../class_d_d_m.html#af6f4cab6e2175c4864222709f0d6bad4',1,'DDM::fitModelMLE()']]],
   ['fixationdata_1',['FixationData',['../class_fixation_data.html#a79013248039f4078807b770647c9d3c4',1,'FixationData']]]
 ];
